6a90f90f3933e8691e0fa7e9146d3843ee1773d6
Daniel Kenyon-Jones <github@dlkj.co.uk>
2022-10-16 16:30:08 +0100

    Move imu and mag code into lib

--- src/bin/serial.rs
+++ src/bin/serial.rs
@@ -1,22 +1,21 @@
 #![no_std]
 #![no_main]
 #![warn(clippy::pedantic, clippy::nursery)]
+#![allow(clippy::missing_errors_doc)]
 
 use ahrs::{Ahrs, Madgwick};
 use bsp::entry;
 use bsp::hal;
-use core::f32::consts::PI;
 use core::fmt::Write;
 use defmt::{error, info};
 use defmt_rtt as _;
-use embedded_hal::blocking::i2c;
-use embedded_hal::blocking::i2c::SevenBitAddress;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::digital::v2::ToggleableOutputPin;
 use embedded_hal::timer::CountDown;
 use fugit::ExtU32;
 use fugit::RateExtU32;
 use hal::{clocks::init_clocks_and_plls, pac, sio::Sio, watchdog::Watchdog};
+use imu_playground::Imc20948;
 use nalgebra::{UnitQuaternion, Vector3};
 use num_traits::ops::euclid::Euclid;
 use panic_probe as _;
@@ -24,9 +23,6 @@ use rp_pico as bsp;
 #[allow(clippy::wildcard_imports)]
 use usb_device::{class_prelude::*, prelude::*};
 use usbd_serial::SerialPort;
-
-const IMU_ADDR: SevenBitAddress = 0x68;
-const MAG_ADDR: SevenBitAddress = 0x0c;
 
 #[entry]
 fn main() -> ! {
@@ -61,7 +57,7 @@ fn main() -> ! {
     let sda_pin = pins.gpio14.into_mode::<hal::gpio::FunctionI2C>();
     let scl_pin = pins.gpio15.into_mode::<hal::gpio::FunctionI2C>();
 
-    let mut i2c = hal::I2C::i2c1(
+    let i2c_master = hal::I2C::i2c1(
         pac.I2C1,
         sda_pin,
         scl_pin,
@@ -70,25 +66,22 @@ fn main() -> ! {
         &clocks.peripheral_clock,
     );
 
-    let imu_id = imu_who_am_i(&mut i2c).unwrap();
-    assert_eq!(
-        imu_id, 0xEA,
-        "Unexpected i2c device id {:X}, expected 0xEA",
-        imu_id
-    );
+    let mut imc = Imc20948::new(i2c_master);
 
-    imu_enable_i2c_bypass(&mut i2c).unwrap();
+    imc.startup().unwrap();
 
-    let mag_id = mag_who_am_i(&mut i2c).unwrap();
+    imc.imu_enable_i2c_bypass().unwrap();
+
+    let mag_id = imc.mag_who_am_i().unwrap();
     assert_eq!(
         mag_id, 0x0948,
         "Unexpected i2c device id {:X}, expected 0xEA",
         mag_id
     );
 
-    imu_wake(&mut i2c).unwrap();
+    imc.imu_wake().unwrap();
 
-    mag_wake(&mut i2c).unwrap();
+    imc.mag_wake().unwrap();
 
     let usb_alloc = UsbBusAllocator::new(hal::usb::UsbBus::new(
         pac.USBCTRL_REGS,
@@ -116,20 +109,30 @@ fn main() -> ! {
 
     let mut ahrs = Madgwick::<f32>::new(0.1, 0.1);
 
+    let mut n = 0;
     loop {
         // A welcome message at the beginning
         if log_count_down.wait().is_ok() {
-            let m = mag_read(&mut i2c);
-            let r = imu_read(&mut i2c);
+            let m = imc.mag_read();
+            let r = imc.imu_read();
             if m.is_err() || r.is_err() {
                 continue;
             }
             let (gyro, acc) = r.unwrap();
-            let mag = m.unwrap();
+            let rm = m.unwrap();
 
-            let quat = ahrs.update(&gyro, &acc, &mag).unwrap();
+            n += 1;
+            if n > 20 {
+                info!(
+                    "acc: {},{},{}, mag: {},{},{}",
+                    acc.x, acc.y, acc.z, rm.x, rm.y, rm.z
+                );
+                n = 0;
+            }
 
-            write_to_serial(&mut serial, &mut led_pin, acc, quat);
+            let quat = ahrs.update(&gyro, &acc, &rm).unwrap();
+
+            write_to_serial(&mut serial, &mut led_pin, acc, rm, quat);
         }
 
         // Check for new data
@@ -149,6 +152,7 @@ fn write_to_serial<U: UsbBus, P: ToggleableOutputPin + OutputPin>(
     serial: &mut SerialPort<U>,
     led_pin: &mut P,
     acc: Vector3<f32>,
+    mag: Vector3<f32>,
     quat: &UnitQuaternion<f32>,
 ) {
     let (roll, pitch, yaw) = quat.euler_angles();
@@ -158,10 +162,13 @@ fn write_to_serial<U: UsbBus, P: ToggleableOutputPin + OutputPin>(
     let mut s = heapless::String::<256>::new();
     core::write!(
         &mut s,
-        "/*{},{},{},{},{},{}*/\r\n",
+        "/*{},{},{},{},{},{},{},{},{}*/\r\n",
         acc.x,
         acc.y,
         acc.y,
+        mag.x,
+        mag.y,
+        mag.z,
         (roll.to_degrees()).rem_euclid(&deg),
         (pitch.to_degrees()).rem_euclid(&deg),
         (yaw.to_degrees()).rem_euclid(&deg)
@@ -173,75 +180,4 @@ fn write_to_serial<U: UsbBus, P: ToggleableOutputPin + OutputPin>(
     } else {
         led_pin.set_low().ok();
     }
-}
-
-fn imu_who_am_i<I: i2c::WriteRead>(i2c: &mut I) -> Result<u8, I::Error> {
-    let mut buffer = [1];
-    //who am i?
-    i2c.write_read(IMU_ADDR, &[0u8], &mut buffer)?;
-    //expect EA
-    info!("ID: {:X}", buffer);
-    Ok(buffer[0])
-}
-
-fn mag_who_am_i<I: i2c::WriteRead>(i2c: &mut I) -> Result<u16, I::Error> {
-    let mut buffer = [0; 2];
-    //who am i?
-    i2c.write_read(MAG_ADDR, &[0u8], &mut buffer)?;
-    //expect EA
-    info!("ID: {:X}", buffer);
-    Ok(u16::from_le_bytes([buffer[0], buffer[1]]))
-}
-
-fn imu_enable_i2c_bypass<I: i2c::Write>(i2c: &mut I) -> Result<(), I::Error> {
-    //Enable BYPASS_EN
-    i2c.write(IMU_ADDR, &[0xF, 0x02])
-}
-
-fn imu_wake<I: i2c::Write>(i2c: &mut I) -> Result<(), I::Error> {
-    //wake from sleep
-    i2c.write(IMU_ADDR, &[0x6, 0x1])
-}
-
-fn mag_wake<I: i2c::Write>(i2c: &mut I) -> Result<(), I::Error> {
-    //enable 100hz read
-    i2c.write(MAG_ADDR, &[0x31, 0x8])
-}
-
-fn imu_read<I: i2c::WriteRead>(i2c: &mut I) -> Result<(Vector3<f32>, Vector3<f32>), I::Error> {
-    let mut buffer = [0; 12];
-
-    i2c.write_read(IMU_ADDR, &[0x2Du8], &mut buffer)?;
-
-    let acc_x = f32::from(i16::from_be_bytes([buffer[0], buffer[1]]));
-    let acc_y = f32::from(i16::from_be_bytes([buffer[2], buffer[3]]));
-    let acc_z = f32::from(i16::from_be_bytes([buffer[4], buffer[5]]));
-
-    let gyr_x = f32::from(i16::from_be_bytes([buffer[6], buffer[7]]));
-    let gyr_y = f32::from(i16::from_be_bytes([buffer[8], buffer[9]]));
-    let gyr_z = f32::from(i16::from_be_bytes([buffer[10], buffer[11]]));
-
-    let gyro = Vector3::new(gyr_x, gyr_y, gyr_z) * (PI / 180.0) / 131.0;
-
-    let acc = Vector3::new(acc_x, acc_y, acc_z) / 16384.0;
-    Ok((gyro, acc))
-}
-
-fn mag_read<I: i2c::WriteRead>(i2c: &mut I) -> Result<Vector3<f32>, I::Error> {
-    let mut buffer = [0; 9];
-
-    i2c.write_read(MAG_ADDR, &[0x10], &mut buffer)?;
-
-    //let status1 = buffer[0];
-
-    let mag_x = f32::from(i16::from_le_bytes([buffer[1], buffer[2]]));
-    let mag_y = f32::from(i16::from_le_bytes([buffer[3], buffer[4]]));
-    let mag_z = f32::from(i16::from_le_bytes([buffer[5], buffer[6]]));
-
-    //buffer[7] is a dummy register
-
-    //let status2 = buffer[8];
-
-    let mag = Vector3::new(mag_x, mag_y, mag_z);
-    Ok(mag)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,150 @@
 #![no_std]
 #![warn(clippy::pedantic, clippy::nursery)]
+#![allow(clippy::missing_errors_doc)]
+
+use core::{f32::consts::PI, fmt::Debug};
+use defmt::info;
+use embedded_hal::blocking::i2c;
+use nalgebra::Vector3;
+
+const MAG_ADDR: i2c::SevenBitAddress = 0x0c;
+const IMU_ADDR: i2c::SevenBitAddress = 0x68;
+
+pub struct Imc20948<I, E>
+where
+    I: i2c::Read<Error = E> + i2c::Write<Error = E> + i2c::WriteRead<Error = E>,
+{
+    i2c: I,
+}
+
+#[derive(Debug)]
+pub enum ImcError<E> {
+    I2c(E),
+    BadId,
+}
+
+impl<I, E> Imc20948<I, E>
+where
+    I: i2c::Read<Error = E> + i2c::Write<Error = E> + i2c::WriteRead<Error = E>,
+{
+    pub const fn new(i2c: I) -> Self {
+        Self { i2c }
+    }
+
+    pub fn startup(&mut self) -> Result<(), ImcError<E>> {
+        //check id
+        let imu_id = self.imu_who_am_i().map_err(|e| ImcError::I2c(e))?;
+        if imu_id != 0xEA {
+            return Err(ImcError::BadId);
+        }
+
+        //set bank0
+        self.imu_set_bank(0).map_err(|e| ImcError::I2c(e))?;
+
+        //soft reset
+        self.imu_soft_reset().map_err(|e| ImcError::I2c(e))?;
+
+        //wake
+        self.imu_wake().map_err(|e| ImcError::I2c(e))?;
+
+        //full power
+
+        //mag startup
+
+        //non minimal stuff
+        //sample mode
+
+        //set scales
+
+        Ok(())
+    }
+
+    pub fn imu_who_am_i(&mut self) -> Result<u8, E> {
+        let mut buffer = [1];
+        //who am i?
+        self.i2c.write_read(IMU_ADDR, &[0u8], &mut buffer)?;
+        //expect EA
+        info!("ID: {:X}", buffer);
+        Ok(buffer[0])
+    }
+
+    pub fn imu_enable_i2c_bypass(&mut self) -> Result<(), E> {
+        //reset i2c master
+        self.i2c.write(IMU_ADDR, &[0x3, 0x02])?;
+
+        //Enable BYPASS_EN
+        self.i2c.write(IMU_ADDR, &[0xF, 0x02])
+    }
+
+    pub fn imu_wake(&mut self) -> Result<(), E> {
+        //wake from sleep
+        self.i2c.write(IMU_ADDR, &[0x6, 0x1])
+    }
+
+    pub fn imu_read(&mut self) -> Result<(Vector3<f32>, Vector3<f32>), E> {
+        let mut buffer = [0; 12];
+
+        self.i2c.write_read(IMU_ADDR, &[0x2Du8], &mut buffer)?;
+
+        let acc_x = f32::from(i16::from_be_bytes([buffer[0], buffer[1]]));
+        let acc_y = f32::from(i16::from_be_bytes([buffer[2], buffer[3]]));
+        let acc_z = f32::from(i16::from_be_bytes([buffer[4], buffer[5]]));
+
+        let gyr_x = f32::from(i16::from_be_bytes([buffer[6], buffer[7]]));
+        let gyr_y = f32::from(i16::from_be_bytes([buffer[8], buffer[9]]));
+        let gyr_z = f32::from(i16::from_be_bytes([buffer[10], buffer[11]]));
+
+        let gyro = Vector3::new(gyr_x, gyr_y, gyr_z) * (PI / 180.0) / 131.0;
+
+        let acc = Vector3::new(acc_x, acc_y, acc_z) / 16384.0;
+        Ok((gyro, acc))
+    }
+
+    pub fn mag_read(&mut self) -> Result<Vector3<f32>, E> {
+        let mut buffer = [0; 9];
+
+        self.i2c.write_read(MAG_ADDR, &[0x10], &mut buffer)?;
+
+        //let status1 = buffer[0];
+
+        //realign magnetometer axis with imu
+        let mag_x = f32::from(i16::from_le_bytes([buffer[1], buffer[2]]));
+        let mag_y = f32::from(i16::from_le_bytes([buffer[3], buffer[4]]));
+        let mag_z = f32::from(i16::from_le_bytes([buffer[5], buffer[6]]));
+
+        //buffer[7] is a dummy register
+
+        //let status2 = buffer[8];
+
+        Ok(Vector3::new(mag_x, mag_y, mag_z))
+    }
+
+    pub fn mag_who_am_i(&mut self) -> Result<u16, E> {
+        let mut buffer = [0; 2];
+        //who am i?
+        self.i2c.write_read(MAG_ADDR, &[0u8], &mut buffer)?;
+        //expect EA
+        info!("ID: {:X}", buffer);
+        Ok(u16::from_le_bytes([buffer[0], buffer[1]]))
+    }
+
+    pub fn mag_wake(&mut self) -> Result<(), E> {
+        //enable 100hz read
+        self.i2c.write(MAG_ADDR, &[0x31, 0x8])
+    }
+
+    fn imu_soft_reset(&mut self) -> Result<(), E> {
+        let mut buffer = [0; 1];
+        self.i2c.write_read(IMU_ADDR, &[0x06], &mut buffer)?;
+
+        buffer[0] |= 0x01;
+
+        self.i2c.write(IMU_ADDR, &[0x06, 0x01])
+    }
+    fn imu_set_bank(&mut self, bank: u8) -> Result<(), E> {
+        //error if bank > 3
+
+        let bank = (bank << 4) & 0x30;
+        self.i2c.write(IMU_ADDR, &[0x7F, bank])
+    }
+}
