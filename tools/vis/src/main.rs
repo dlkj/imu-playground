ec893740d0924c3d9f6f3c3dbb47b3cef6606e00
Daniel Kenyon-Jones <github@dlkj.co.uk>
2022-10-21 11:33:10 +0100

    Bevy hello world

--- tools/vis/src/main.rs
+++ tools/vis/src/main.rs
@@ -1,5 +1,43 @@
 #![warn(clippy::pedantic, clippy::nursery)]
 #![allow(clippy::missing_errors_doc)]
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn hello_world() {
+    println!("hello world!");
 }
