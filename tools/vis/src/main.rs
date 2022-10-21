254df5159cfafe77901e0b4be724efed4c52c083
Daniel Kenyon-Jones <github@dlkj.co.uk>
2022-10-21 13:14:15 +0100

    Basic bevy rotating cubes

--- tools/vis/src/main.rs
+++ tools/vis/src/main.rs
@@ -3,41 +3,68 @@
 
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Rotator;
+
 fn main() {
     App::new()
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(startup)
+        .add_system(rotator_system)
         .run();
 }
 
-#[derive(Component)]
-struct Person;
+fn startup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
 
-#[derive(Component)]
-struct Name(String);
+    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
+    let cube_material_handle = materials.add(Color::rgb(0.8, 0.7, 0.6).into());
 
-fn add_people(mut commands: Commands) {
     commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Elaina Proctor".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Renzo Hume".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Zayna Nieves".to_string()));
+        .spawn_bundle(PbrBundle {
+            mesh: cube_handle.clone(),
+            material: cube_material_handle.clone(),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        })
+        .insert(Rotator)
+        .with_children(|parent| {
+            parent.spawn_bundle(PbrBundle {
+                mesh: cube_handle,
+                material: cube_material_handle,
+                transform: Transform::from_xyz(1.0, 1.0, 1.0),
+                ..default()
+            });
+        });
+
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    // camera
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotator>>) {
+    for mut transform in &mut query {
+        transform.rotate_y(1.0 * time.delta_seconds());
     }
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
